94543cb98a64b8fd772160c42b78f2afb85f5e12
Achoruz <irvanraharjo321@gmail.com>
2023-01-25 11:02:03 +0700

    simple black jack

---
+++ index.js
@@ -0,0 +1,65 @@
+let player = {
+  name: "Irvan",
+  chips: 145,
+};
+let cards = []; // array -ordered list fof items
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+// let sumEl = document.querySelector(".sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber >= 11) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let j = 0; j < cards.length; j++) {
+    cardsEl.textContent += cards[j] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  // CASH OUT
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let kartu = getRandomCard();
+    sum += kartu;
+    cards.push(kartu);
+    renderGame();
+  }
+}
